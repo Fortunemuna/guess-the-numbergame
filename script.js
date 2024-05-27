a9652cb1631bd9ec6b5553d36e305fdd1f380a42
Dev Muna <166520002+Fortunemuna@users.noreply.github.com>
2024-05-27 10:24:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+  let secretNumber = Math.floor(Math.random() * 100) + 1;
+  let chances = 5;
+
+  function checkGuess() {
+    const userGuess = parseInt(document.getElementById("userGuess").value);
+
+    if (isNaN(userGuess) || userGuess< 1 || userGuess > 100) {
+      document.getElementById('result').innerHTML ="please enter valid number";
+      return; 
+    }
+    
+    chances--;
+
+    if (userGuess === secretNumber){
+        document.getElementById("result").style.color = "green";
+        document.getElementById("result").innerHTML = "Congratulations! You gueesed the correct number!";
+        disableInputAndButton();
+    }else{
+      const hint = userGuess > secretNumber ? "higher":"lower";
+      document.getElementById("result").innerHTML = "Your guess is " + hint;
+      document.getElementById("chances").innerHTML = chances;    
+    }
+    if (chances == 0) {
+      document.getElementById("result").innerHTML = `Sorry, you've run out of chances. The correct number was ${secretNumber}`;
+      disableInputAndButton();
+    }
+   }
+
+
+   function disableInputAndButton() {
+     document.getElementById("userGuess").disabled = true;
+     document.querySelector("button").disabled = true;
+   }
+
+
+   function replay() {
+     secretNumber = Math.floor(Math.random() * 100) + 1;  
+     chances = 5;
+
+     document.getElementById("userGuess").value = "";
+     document.getElementById("result").innerHTML = "";
+     document.getElementById("result").style.color = "#e74c3c";
+     document.getElementById("chances").innerHTML = chances;
+     document.getElementById("userGuess").disabled = false;
+     document.querySelector("button").disabled = false;
+  }
